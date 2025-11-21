e206803f437278d676cf56137fd6b1d2ef8b5d97
Dillshad685 <dillshadb2@gmail.com>
2025-11-21 07:53:08 +0530

    newcommit

--- shell-instance.sh
+++ shell-instance.sh
@@ -7,12 +7,12 @@ SG_ID="sg-00d0eb3bdf7ae9959"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=$instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)  #to create EC2 instance using shell
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)  #to create EC2 instance using shell
 
     #to get private IP
-    if [ instance != "frontend" ]; then
+    if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        RECORD_NAME=$.$"Domain.name"
+        #RECORD_NAME=$.$"Domain.name"
     else 
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     fi
