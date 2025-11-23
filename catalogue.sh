895da67ee017867e8603e7eb024ceeb6ad806dab
Dillshad685 <dillshadb2@gmail.com>
2025-11-23 19:22:47 +0530

    instances

--- catalogue.sh
+++ catalogue.sh
@@ -92,7 +92,7 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "installed mongodb" 
 
 INDEX=$(mongosh mongodb.dillshad.space --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
-if [ $INDEX -ne 0 ]; then
+if [ $INDEX -le 0 ]; then
    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
    VALIDATE $? "Load catalogue products"
 else
